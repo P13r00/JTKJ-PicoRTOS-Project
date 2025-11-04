3d9fbfd43230f9967f089a2a83624256244476d5
P13r0 <piero.cianciotta@gmail.com>
2025-11-04 16:46:40 +0200

    idfk

--- src/items.c
+++ src/items.c
@@ -10,6 +10,7 @@
 #include "tkjhat/sdk.h"
 
 #define DEFAULT_STACK_SIZE 2048
+#define THRESHOLD          100
 
 //states
 enum state { WAITING=0,             //await input from sensor or workstation
@@ -65,6 +66,7 @@ while True:
 
 static void toggle_symbol_detection(uint gpio, uint32_t eventMask) {
     //Change State
+    programState = SYMBOL_DETECTION;
 }
 
 static void append_to_string(char *message, char symbol) {
@@ -74,41 +76,112 @@ static void append_to_string(char *message, char symbol) {
     *message = '\0';
 }
 
-//handlers and tasks
-TaskHandle_t hsymbolDetectionTask, hdisplayingTask, hmsgToWorkstationTask= NULL;
+static void space_key(uint gpio, uint32_t eventMask) {
+    // key to add space
+    append_to_string(current_message, ' ');
+}
 
-BaseType_t  result = xTaskCreate(symbolDetectionTask,
-            "Symbol Detection",
-            DEFAULT_STACK_SIZE,
-            NULL,
-            2,
-            &hSensorTask);
-            if(result != pdPASS) {
-                printf("Symbol Detection task creation failed\n");
-                return 0;
-            }
+static void starting_initializer(){
+    stdio_init_all();
+    init_hat_sdk();
+    sleep_ms(300);
+    init_button1();
+    init_button2();
+    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, toggle_symbol_detection);
+    gpio_set_irq_enabled_with_callback(BUTTON2, GPIO_IRQ_EDGE_RISE, true, space_key);
 
-            result = xTaskCreate(displayingTask,
-                        "Displaying", 
-                        DEFAULT_STACK_SIZE,
-                        NULL,
-                        2,
-                        &hdisplayingTask);
-            if(result != pdPASS) {
-                printf("Displaying Task creation failed\n");
-                return 0;
-            }
+    if (init_ICM42670() != 0) {
+        printf("Failed to initialize ICM42670\n");
+    } else {
+        printf("ICM42670 initialized successfully\n");
+        if (ICM42670_start_with_default_values() != 0) {
+            printf("ICM42670 start-with-defaults failed\n");
+        } else {
+            printf("ICM42670 started with default values\n");
+        }
+    }
+    sleep_ms(100);
+}
 
-            result = xTaskCreate(msgToWorkstationTask,  //could be optional and included in the decoding function
-                        "Displaying", 
-                        DEFAULT_STACK_SIZE,
-                        NULL,
-                        2,
-                        &hmsgToWorkstationTask);
-            if(result != pdPASS) {
-                printf("Message to Workstation Task creation failed\n");
-                return 0;
+static void symbolDetectionTask(void *pvParameters) {
+    if(programState==SYMBOL_DETECTION){
+        for (;;) {
+        if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) != 0) {
+            printf("Sensor read failed:");
+            sleep_ms(200);
+            continue;
+        }
+        if (!(gx >= THRESHOLD || gx <= -THRESHOLD)) {
+           gx = 0;
+        }
+        float diff = gx - last_printed_gx;
+        if (diff < 0.0f) diff = -diff;
+        if (first_gx || diff >= 20.0f) {
+            if(gx >= THRESHOLD) {
+                append_to_string(current_message, '.'); //down
+            } else if (gx <= -THRESHOLD) {
+                append_to_string(current_message, '-'); //up
             }
+            last_printed_gx = gx;
+            first_gx = 0;
+        }
+        sleep_ms(100);
+        }
+    }
+}
+
+static void displayingTask(void *pvParameters) {
+}
+
+static void msgToWorkstationTask(void *pvParameters) {
+    if(current_message!=0){
+        ("Message: %s", current_message);
+    }
+}
+
+int main(){
+    TaskHandle_t hsymbolDetectionTask, hdisplayingTask, hmsgToWorkstationTask= NULL;
+
+    BaseType_t  result = xTaskCreate(symbolDetectionTask,
+        "Symbol Detection",
+        DEFAULT_STACK_SIZE,
+        NULL,
+        2,
+        &hSensorTask);
+        if(result != pdPASS) {
+            printf("Symbol Detection task creation failed\n");
+            return 0;
+        }
+
+        result = xTaskCreate(displayingTask,
+                    "Displaying", 
+                    DEFAULT_STACK_SIZE,
+                    NULL,
+                    2,
+                    &hdisplayingTask);
+        if(result != pdPASS) {
+            printf("Displaying Task creation failed\n");
+            return 0;
+        }
+
+        result = xTaskCreate(msgToWorkstationTask,  //could be optional and included in the decoding function
+                    "Displaying", 
+                    DEFAULT_STACK_SIZE,
+                    NULL,
+                    2,
+                    &hmsgToWorkstationTask);
+        if(result != pdPASS) {
+            printf("Message to Workstation Task creation failed\n");
+            return 0;
+        }
+
+    starting_initializer();
+
+    vTaskStartScheduler();
+
+    return 0
+
+}
 
 
 
