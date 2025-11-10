72338fec42cf28ab83cc72374163288f8fa76ab6
P13r00 <80446661+P13r00@users.noreply.github.com>
2025-11-10 17:41:33 +0200

    State machine working
    Todo: Fix input from console sent to pico
    fix button buffer time
    add display functionalities

--- src/main.c
+++ src/main.c
@@ -12,11 +12,11 @@
 //states
 enum state { WAITING=0,             //await input from sensor or workstation
              SYMBOL_DETECTION=1,    //read symbols from sensor
-             DATA_READY=2,          //string successfully created
-             DECODING=3,            //from Morse -> Characters
-             ENCODING=4,            //from Characters -> Morse
-             DISPLAYING=5,          //displaying the message, providing audio feedback
-             MSG_TO_WORKSTATION=6  //??notneeded?? sending the message to the workstation
+             //DATA_READY=2,          //string successfully created
+             //DECODING=3,            //from Morse -> Characters
+             //ENCODING=4,            //from Characters -> Morse
+             DISPLAYING=3,          //displaying the message, providing audio feedback
+             MSG_FROM_WORKSTATION=2  //??notneeded?? sending the message to the workstation
              //MSG_TO_FRIEND=7,       //sending a message to another project
              //MSG_FROM_FRIEND=8      //receiving a message from another project
             };
@@ -31,49 +31,43 @@ float ax = 0.0, ay = 0.0, az = 0.0, gx = 0.0, gy = 0.0, gz = 0.0, t = 0.0;
 static float last_printed_gx = 0.0f;
 static int first_gx = 0;
 
-/*
-main loop
-while True:
-    if SENDING:
-        SYMBOL_DETECTION
-        DATA_READY
-        DECODING
-        DISPLAYING
-        MSG_TO_WORKSTATION
-
-    else if RECEIVING:
-        DECODING
-        DISPLAYING
-        MSG_TO_WORKSTATION
-
-    else if COMMUNICATING:
-        if SENDING:
-            SYMBOL_DETECTION
-            DATA_READY
-            MSG_TO_FRIEND
-            DECODING
-            DISPLAYING
-            MSG_TO_WORKSTATION
-
-        else if RECEIVING:
-            MSG_FROM_FRIEND
-            DECODING
-            DISPLAYING
-            MSG_TO_WORKSTATION
-
-    WAITING
-*/
-
-static void toggle_symbol_detection(uint gpio, uint32_t eventMask);
+static void button_function(uint gpio, uint32_t eventMask);
 static void append_to_string(char *message, char symbol);
 static void space_key(uint gpio, uint32_t eventMask);
 static void symbolDetectionTask(void *pvParameters);
 static void displayingTask(void *pvParameters);
 static void msgToWorkstationTask(void *pvParameters);
+static void state_change();
 
-static void toggle_symbol_detection(uint gpio, uint32_t eventMask) {
-    //Change State
-    programState = SYMBOL_DETECTION;
+static void button_function(uint gpio, uint32_t eventMask) {
+    if (gpio == BUTTON1) {
+        if(programState == WAITING){
+            programState = SYMBOL_DETECTION;
+            printf("Current State: %d\n", programState);
+        } else {
+            state_change();
+        }
+    } else if (gpio == BUTTON2) {
+        if(programState == WAITING){
+            printf("Changing state to MSG_FROM_WORKSTATION\n");
+            programState = MSG_FROM_WORKSTATION;
+            printf("Current State: %d\n", programState);
+        } else {
+            state_change();
+        }
+    }
+}
+
+static void state_change(){
+    if(programState == SYMBOL_DETECTION||programState == MSG_FROM_WORKSTATION){
+        programState = DISPLAYING;
+        printf("Changing state to DISPLAYING\n");
+    } else if(programState == DISPLAYING){
+        current_message[0] = '\0';
+        programState = WAITING;
+        printf("Changing state to WAITING\n");
+    }
+    //printf("Current State: %d\n", programState);
 }
 
 static void append_to_string(char *message, char symbol) {
@@ -84,13 +78,12 @@ static void append_to_string(char *message, char symbol) {
 }
 
 static void space_key(uint gpio, uint32_t eventMask) {
-    // key to add space
     append_to_string(current_message, ' ');
 }
 
 static void symbolDetectionTask(void *pvParameters) {
     while(1){
-        if(programState==SYMBOL_DETECTION){
+        if(programState == SYMBOL_DETECTION){
             for (;;) {
                 if (ICM42670_read_sensor_data(&ax, &ay, &az, &gx, &gy, &gz, &t) != 0) {
                     printf("Sensor read failed:");
@@ -118,13 +111,46 @@ static void symbolDetectionTask(void *pvParameters) {
 }
 
 static void displayingTask(void *pvParameters) {
-    while(1){}
+    /*(void)pvParameters;
+
+    init_display();
+    printf("Initializing display\n");
+    static int counter = 0; 
+
+    while(1) {
+        clear_display();
+        char buf[5]; //Store a number of maximum 5 figures 
+        sprintf(buf,"%d",counter++);
+        write_text(buf);
+        vTaskDelay(pdMS_TO_TICKS(4000));
+    }*/
+   while(1){}
+
 }
 
 static void msgToWorkstationTask(void *pvParameters) {
     while(1){
-        if(current_message!=0){
-            ("Message: %s", current_message);
+        if(programState == DISPLAYING){
+            printf("Message: %s\n", current_message);
+            state_change(&programState);
+            vTaskDelay(pdMS_TO_TICKS(100));
+        }
+    }
+}
+
+static void msgFromWorkstationTask(void *pvParameters) {
+    while(1){
+        if(programState == MSG_FROM_WORKSTATION){
+            printf("Write your message to send to the Pico: ");
+            /* wait until a non-empty message has been received */
+            current_message[0] = '\0';
+            while (current_message[0] == '\0') {
+                if (scanf("%255s", current_message) == 1){ // implement i2c_read 
+                    break;
+                };
+                vTaskDelay(pdMS_TO_TICKS(100));
+            }
+            state_change(&programState);
         }
     }
 }
@@ -132,17 +158,18 @@ static void msgToWorkstationTask(void *pvParameters) {
 int main(){
     stdio_init_all();
     init_hat_sdk();
-    sleep_ms(20000);
+    sleep_ms(5000);
     init_sw1();
     init_sw2();
-    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, toggle_symbol_detection);
-    gpio_set_irq_enabled_with_callback(BUTTON2, GPIO_IRQ_EDGE_RISE, true, space_key);
+    gpio_set_irq_enabled_with_callback(BUTTON1, GPIO_IRQ_EDGE_RISE, true, button_function);
+    gpio_set_irq_enabled(BUTTON2, GPIO_IRQ_EDGE_RISE, true);
 
     init_ICM42670();
     ICM42670_start_with_default_values();
-    printf("Init successful");   
+    printf("Init successful\n");   
     sleep_ms(100);
-    TaskHandle_t hsymbolDetectionTask, hdisplayingTask, hmsgToWorkstationTask;
+    printf("Start by pressing Button1 (right button)\n");
+    TaskHandle_t hsymbolDetectionTask, hdisplayingTask, hmsgToWorkstationTask, hmsgFromWorkstationTask = NULL;
 
     BaseType_t  result = xTaskCreate(symbolDetectionTask,
         "Symbol Detection",
@@ -165,28 +192,15 @@ int main(){
                     2,
                     &hmsgToWorkstationTask);
 
-    
-    
+        result = xTaskCreate(msgFromWorkstationTask,
+                    "Receiving from workstation", 
+                    DEFAULT_STACK_SIZE,
+                    NULL,
+                    2,
+                    &hmsgFromWorkstationTask);
 
+    
     vTaskStartScheduler();
-    // stdio_init_all();
-    // sleep_ms(20000);  // Wait for serial
-    // printf("=== Starting Program ===\n");
     
-    // init_hat_sdk();
-    // printf("HAT SDK initialized\n");
-    
-    // // Check IMU initialization
-    // int imu_result = init_ICM42670();
-    // printf("IMU init result: %d\n", imu_result);
-    
-    // if (imu_result == 0) {
-    //     int start_result = ICM42670_start_with_default_values();
-    //     printf("IMU start result: %d\n", start_result);
-    // }
-    
-    // printf("Initialization complete\n");
-
-    // return 0;
-
+    return 0;
 }
